0697a16699842cfd07c6225a2b711f454927f4e8
Anders Kvist <ak@cego.dk>
2019-06-07 15:46:57 +0200

    Initial add of code to access RenoWeb and get address id and get plan for trash pickups.

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	"github.com/anderskvist/GoRenoWeb2MQTT/renoweb"
+
+	"github.com/anderskvist/DVIEnergiSmartControl/log"
+	ini "gopkg.in/ini.v1"
+)
+
+func main() {
+	cfg, err := ini.Load(os.Args[1])
+
+	if err != nil {
+		log.Criticalf("Fail to read file: %v", err)
+		os.Exit(1)
+	}
+
+	poll := cfg.Section("main").Key("poll").MustInt(60)
+	log.Infof("Polltime is %d seconds.\n", poll)
+	for t := range time.NewTicker(time.Duration(poll) * time.Second).C {
+		log.Notice("Tick")
+		if t == t {
+		}
+		log.Info("Getting data from RenoWeb")
+		addressID := renoweb.GetRenoWebAddressID(cfg)
+		renoweb.GetRenoWebPickupPlan(addressID)
+		//dviData := dvi.GetDviData(cfg)
+		log.Info("Done getting data from RenoWeb")
+
+		log.Info("Sending data to MQTT")
+		//mqtt.SendToMQTT(cfg, dviData)
+		log.Info("Done sending to MQTT")
+		os.Exit(0)
+	}
+}
---
+++ renoweb/renoweb.go
@@ -0,0 +1,118 @@
+package renoweb
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/anderskvist/DVIEnergiSmartControl/log"
+	ini "gopkg.in/ini.v1"
+)
+
+type AddressSearchSearch struct {
+	Searchterm          string `json:"searchterm"`
+	Addresswithmateriel int    `json:"addresswithmateriel"`
+}
+
+// D is a temporary struct to keep data from renoweb
+type D struct {
+	D string `json:"d"`
+}
+
+type AddressSearch struct {
+	List   []AddressSearchList
+	Status AddressSearchStatus
+}
+
+type AddressSearchList struct {
+	Value int    `json:"value,string"`
+	Label string `json:"label"`
+}
+
+type AddressSearchStatus struct {
+	ID     int    `json:"id"`
+	Status string `json:"status"`
+	Msg    string `json:"msg"`
+}
+
+type PickupPlanSearch struct {
+	Adrid  int  `json:"adrid"`
+	Common bool `json:"common"`
+}
+
+type PickupPlan struct {
+	List []PickupPlanList
+}
+
+type PickupPlanList struct {
+	ID            int    `json:"id"`
+	MaterielNavn  string `json:"materielnavn"`
+	OrdningNavn   string `json:"ordningnavn"`
+	ToemningsDage string `json:"toemningsdage"`
+	ToemningsDato string `json:"toemningsdato"`
+}
+
+func jsonPrettyPrint(in string) string {
+	var out bytes.Buffer
+	err := json.Indent(&out, []byte(in), "", "\t")
+	if err != nil {
+		return in
+	}
+	return out.String()
+}
+
+func GetRenoWebAddressID(cfg *ini.File) int {
+	search := AddressSearchSearch{
+		Searchterm:          cfg.Section("renoweb").Key("address").String(),
+		Addresswithmateriel: 3}
+
+	jsondata, _ := json.Marshal(search)
+	log.Debugf("%s\n", jsonPrettyPrint(string(jsondata)))
+
+	response, _ := http.Post("https://rebild-sb.renoweb.dk/Legacy/JService.asmx/Adresse_SearchByString", "application/json", bytes.NewBuffer(jsondata))
+	data, _ := ioutil.ReadAll(response.Body)
+	log.Debugf("%s\n", jsonPrettyPrint(string(data)))
+
+	var d D
+	err := json.Unmarshal(data, &d)
+	if err != nil {
+		panic(err)
+	}
+	log.Debugf("%s\n", d.D)
+
+	var addressSearch AddressSearch
+	err = json.Unmarshal([]byte(d.D), &addressSearch)
+	if err != nil {
+		panic(err)
+	}
+	log.Infof("%#v\n", addressSearch)
+	return addressSearch.List[0].Value
+}
+
+func GetRenoWebPickupPlan(id int) {
+	search := PickupPlanSearch{
+		Adrid:  id,
+		Common: false}
+
+	jsondata, _ := json.Marshal(search)
+	log.Debugf("%s\n", jsonPrettyPrint(string(jsondata)))
+
+	response, _ := http.Post("https://rebild-sb.renoweb.dk/Legacy/JService.asmx/GetAffaldsplanMateriel_mitAffald", "application/json", bytes.NewBuffer(jsondata))
+	data, _ := ioutil.ReadAll(response.Body)
+	log.Debugf("%s\n", jsonPrettyPrint(string(data)))
+
+	var d D
+	err := json.Unmarshal(data, &d)
+	if err != nil {
+		panic(err)
+	}
+	log.Debugf("%s\n", d.D)
+
+	var pickupPlan PickupPlan
+	err = json.Unmarshal([]byte(d.D), &pickupPlan)
+	if err != nil {
+		panic(err)
+	}
+	log.Infof("%#v\n", pickupPlan)
+}
